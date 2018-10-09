6136071424fe995303db3fee9d08e00ed21d26b4
Sylvain Gogel <sylvain.gogel@gmail.com>
2018-10-09 17:40:23 +0200

    (chore:config) fix export of the bundle to jekyll src folder

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,7 @@ module.exports = {
     // we’re going to put the generated file in the assets folder so jekyll will grab it.
     // if using GitHub Pages, use the following:
     // path: "assets/javascripts"
-    path: __dirname + "src/js/",
+    path: path.resolve(__dirname, "src/js"),
     filename: "bundle.js"
   },
   module: {
